7ce1c93b0e8e7dfed6d2c86be8f73d5e64d1ace1
peterhutwelker <phutwelker@gmail.com>
2026-09-04 20:04:21 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='debbi-tracker-v1';
-const FILES=['./','./index.html','./manifest.json'];
+const CACHE='debbi-tracker-v3';
+const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
