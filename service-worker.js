ae312a472835980ce92c523ad3e436ce10956490
peterhutwelker <phutwelker@gmail.com>
2026-09-04 20:13:47 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,19 @@
-const CACHE='debbi-tracker-v3';
-const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='debbi-tracker-v4';
+const FILES=['./','./index.html','./manifest-v2.json','./debbi-icon-192.png','./debbi-icon-512.png'];
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+    self.clients.claim()
+  ]));
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(fetch(e.request).then(r=>{
+    const copy=r.clone();
+    caches.open(CACHE).then(c=>c.put(e.request,copy));
+    return r;
+  }).catch(()=>caches.match(e.request)));
+});
